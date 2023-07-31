7d293dfe75ec96831494426d5b5d24a8d99140cc
Thibaut Nijs <contact@thibautnijs.be>
2023-07-31 16:59:16 +0200

    Fix tutorial page with bare swiper.js swipe implementation

--- src/app/pages/tutorial/tutorial.module.ts
+++ src/app/pages/tutorial/tutorial.module.ts
@@ -1,11 +1,15 @@
 import { TranslateModule } from '@ngx-translate/core'
 import { RouterModule } from '@angular/router'
-import { NgModule } from '@angular/core'
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 
 import { IonicModule } from '@ionic/angular'
 import { TutorialPage } from './tutorial.page'
+
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @NgModule({
   imports: [
@@ -15,8 +19,9 @@ import { TutorialPage } from './tutorial.page'
     TranslateModule.forChild(),
     RouterModule.forChild([{
       path: '', component: TutorialPage
-    }])
+    }]),
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [TutorialPage]
 })
 export class TutorialPageModule { }
