22ed37861d946217cf69360733be516fbb42ad88
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2025-08-28 15:41:52 +0200

    update versionnum

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '2.0.6',
+  version: '2.0.7',
   default_language: 'nl-BE',
   supported_languages: [
     'nl-BE',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  version: '2.0.6-dev',
+  version: '2.0.7-dev',
   default_language: 'nl-BE',
   supported_languages: [
     'nl-BE',
